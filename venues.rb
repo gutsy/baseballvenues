9fa3e62d336f22e778aae0b678d68856a8d19485
Todd Guerra <tguerra@crownpartners.com>
2013-07-28 21:47:38 -0400

    initial commit of my prelim work with ruby

---
+++ venues.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+$key= ""
+
+ids = {
+	'reds' => '17',
+	'mariners' => '12',
+	'cubs' => '16'
+}
+
+get '/venue/:team' do
+
+	$id = ids[params[:team]]
+	"Looking for the venue of the #{params[:team]}, which has id " + $id
+end
