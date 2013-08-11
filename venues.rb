66e937e30728dc14e7b6dfe29fde0e3887db2499
Todd M. Guerra <toddguerra@gmail.com>
2013-08-11 16:01:43 -0400

    Update venues.rb
    My Ruby/Sinatra experiment is over.

--- venues.rb
+++ venues.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'rubygems'
 require 'net/http'
 require 'json'
 
@@ -12,13 +13,28 @@ ids = {
 
 get '/venue/:team' do
 
+	#find the id from the team string. Only 3 work at the moment
 	id = ids[params[:team]]
 	url = 'http://api.espn.com/v1/sports/baseball/mlb/teams/' + id + '?enable=venues&apikey=' + $key
+	#call ESPN and demand the data. DEMAND IT
 	resp = Net::HTTP.get_response(URI.parse(url))
 	data = resp.body
-	parsed = JSON.parse(data)
+	json = JSON.parse data
 
-	#venue parsed["sports"]["venues"]
-	"Looking for the venue of the #{params[:team]}, which has id " + id + ", and here's the data returned: " + data
+	#then iterate through the JSON to get the venue name
+	venues = json["sports"].first["leagues"]
+              .first["teams"]
+              .each_with_object([]){|h,xs|
+                venues = h["venues"].map{|h| h["name"]}.join(", ")
+                xs << "#{venues}"
+              }
+
+    #this is probably an ugly java-style hack to cut off the array formatting, but oh well
+    venue = venues.to_s
+    venue.gsub!("[\"", "")
+    venue.gsub!("\"]", "")
+
+    #have Sinatra print it
+    "The #{params[:team]} play in " + venue.to_s
 
 end
